46b1fc25de4581fe9a4520b934c808d0a41991a6
Niloo <niloofargst@gmail.com>
2022-01-30 15:41:45 +0100

    Updates

---
+++ precache-manifest.cbc31730dbc1fd96e8d575852284a277.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc9bf5a994b70c89803b12666e6c3ff",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "d208897b224cfb6bf8d4",
+    "url": "/project1/static/css/main.4662149a.chunk.css"
+  },
+  {
+    "revision": "a572f253411ea9ced304",
+    "url": "/project1/static/js/2.19f6b676.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project1/static/js/2.19f6b676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d208897b224cfb6bf8d4",
+    "url": "/project1/static/js/main.e7fe29d0.chunk.js"
+  },
+  {
+    "revision": "5c22081b7156ee992445",
+    "url": "/project1/static/js/runtime-main.b54441be.js"
+  },
+  {
+    "revision": "a08625396466835399192b2fcf8b67e9",
+    "url": "/project1/static/media/coin.a0862539.gif"
+  },
+  {
+    "revision": "10979309f018d8715af6b143c2a8f627",
+    "url": "/project1/static/media/snake.10979309.gif"
+  }
+]);
